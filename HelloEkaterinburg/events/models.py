adb40cce0c3654efe2084e90ebf57016405a4707
katanabana <katanabana01@gmail.com>
2023-08-12 17:41:09 +0500

    html tags on events page for event fields added

--- HelloEkaterinburg/events/models.py
+++ HelloEkaterinburg/events/models.py
@@ -4,18 +4,18 @@ from django.db.models import DateTimeField, CharField
 
 class Event(models.Model):
     CATEGORY = [
-    ["Sport", "Sport"],
-    ["Culture", "Culture"],
-    ["Education", "Education"],
-    ["Hobby", "Hobby"]
+    ["Sport", "Спорт"],
+    ["Culture", "Культура"],
+    ["Education", "Образование"],
+    ["Hobby", "Хобби"]
     ]
     time_create = DateTimeField("Date of creation", auto_now_add=True, null=True, blank=True)
     time_update = DateTimeField("Date of update", auto_now=True, null=True, blank=True)
     # fields:
     name = CharField("Name", max_length=255, null=True, blank=True)
     category = models.CharField("Category", max_length=255, null=True, blank=True, choices=CATEGORY)
-    description = models.CharField("Description", max_length=255, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    description = models.TextField("Description", max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to='event_images', null=True, blank=True)
     start_datetime = models.DateTimeField(null=True, blank=True)
     end_datetime = models.DateTimeField(null=True, blank=True)
     place = models.CharField(max_length=255, blank=True)
