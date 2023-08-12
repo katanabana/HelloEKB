4d675507bacedbcb2f4d2293de8745da4f07a32e
risa <anikat5@yandex.ru>
2023-08-12 13:38:49 +0500

    main page added, static file dir added

--- HelloEkaterinburg/events/urls.py
+++ HelloEkaterinburg/events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', events, name='events')
+    path('events/', events, name='events')
 
 ]
 """path('<pk:event_pk>', EventDetail.as_view(), name='event-detail')"""
--- HelloEkaterinburg/main/views.py
+++ HelloEkaterinburg/main/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+
+def main(request):
+    context = {}
+    return render(request, "main/main.html", context)
