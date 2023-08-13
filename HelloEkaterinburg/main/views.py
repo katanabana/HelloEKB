affae56b4034fa51ba3b87938890b237eeaf0eaa
lizat <anikat5@yandex.ru>
2023-08-13 10:46:22 +0500

    add css to events page

--- HelloEkaterinburg/main/views.py
+++ HelloEkaterinburg/main/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 
+from events.models import Event
+
 
 def main(request):
-    context = {}
+    sport = Event.get_last_from("Sport")
+    culture = Event.get_last_from("Culture")
+    education = Event.get_last_from("Education")
+    hobby = Event.get_last_from("Hobby")
+    context = {"sport": sport, "culture": culture, "education": education, "hobby": hobby}
+    print(context)
     return render(request, "main/main.html", context)
